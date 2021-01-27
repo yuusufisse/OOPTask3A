dd02a71b7cc4ef471c64e6f05bc210d9adfb2182
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-01-27 10:16:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOPTask3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stillMore = false;
+            do
+            {
+                Console.Write("First name: ");
+                string firstname = Console.ReadLine();
+                Console.Write("Family name: ");
+                string familyname = Console.ReadLine();
+                Console.Write("Birthday: ");
+                string isThisValid = Console.ReadLine();
+                DateTime dateofbirth;
+                while(!DateTime.TryParse(isThisValid, out dateofbirth) || 
+                    dateofbirth<=new DateTime(1900,1,1))
+                {
+                    Console.Write("Not valid, try again: ");
+                    isThisValid = Console.ReadLine();
+                }
+                //Now you have it all!!!
+                //Create the object of type Student
+                Student student = new Student(firstname, familyname, dateofbirth);
+
+                // this may not work immediatly...
+                Console.WriteLine(student);
+                //Console.WriteLine("First name: "+student.FirstName);
+
+
+                Console.Write("More of this Y/N?: ");
+                string youDecided = Console.ReadLine().ToUpper();
+                if (youDecided.StartsWith("Y"))
+                    stillMore = true;
+                else
+                    stillMore = false;
+            }while(stillMore);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace OOPTask3A
+{
+    class Student
+    {
+        // Constructor with 3 parameters
+        public Student(string firstName, string familyName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            FamilyName = familyName;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public override string ToString()
+        {
+            //You can define here...
+            CultureInfo culture = new CultureInfo("fi-FI");
+            return $"Firstname is {FirstName}, Family name is {FamilyName}, " +
+                $"Birthday is {string.Format(culture,"{0:D}", DateOfBirth)}";
+        }
+
+    }
+
+}
